6eb63f0b1959ccb243cfe03137d65759457faab2
Guillermo Barberan <guillermo.barberan-ext@dominion-global.com>
2022-06-27 10:42:30 -0600

    modificar

--- src/app/COMPONENTESRUTAS/modificar/modificar.component.ts
+++ src/app/COMPONENTESRUTAS/modificar/modificar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {RutasTransporteService, Rutas} from '../../SERVICES/rutas-transporte.service'
+import {Router, ActivatedRoute} from '@angular/router'
+import {EquipoService, Equipo} from '../../SERVICES/equipo.service'
 
 @Component({
   selector: 'app-modificar',
@@ -6,10 +9,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modificar.component.css']
 })
 export class ModificarComponentRutas implements OnInit {
+//variable
+ListarEquipo:Equipo[] | undefined;
 
-  constructor() { }
+  rutas: Rutas={
+    idRuta:"",
+    direccionOrigen:"",
+    direcciondestino:"",
+    idMaquina:"",
+    costoRuta:""
+  }
+
+  constructor(private RutasService: RutasTransporteService, private router:Router, private activatedRoute:ActivatedRoute, private EquipoService:EquipoService) { }
 
   ngOnInit(): void {
+
+    this.listarEquipo();
+
+    const id_entrada = <string>this.activatedRoute.snapshot.params['id'];
+    
+    console.log('id de entrada: '+id_entrada);
+    
+
+    
+
+    if(id_entrada){
+      this.RutasService.getUnRutas(id_entrada).subscribe(
+        res=>{
+          this.rutas = res[0];
+          console.log(res[0]);
+          
+        },
+        err=>console.log(err)
+      );
+    }
+  }
+  
+
+  listarEquipo(){
+    this.EquipoService.getEquipos().subscribe(
+      res=>{
+        console.log(res);
+        this.ListarEquipo=<any>res;
+      },
+      err => console.log(err)
+    )  
+  }
+
+  modificar()
+  {
+
+    this.RutasService.editRutas(this.rutas.idRuta, this.rutas).subscribe(
+      res=>{
+        console.log(res);
+      },
+      err=>console.log(err)
+    );
+
+    this.router.navigate(['/inicioRutas']);
   }
 
 }
